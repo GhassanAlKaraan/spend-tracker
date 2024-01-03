99014e72758df63c94fce6bab7cc20c5a39919d0
Ghassan Al Karaan <ghass.karaan@gmail.com>
2024-01-03 12:15:25 +0200

    fix

--- lib/pages/2_paths_routing/book_list_page.dart
+++ lib/pages/2_paths_routing/book_list_page.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_template/resources/color_manager.dart';
-import 'package:flutterfire_template/resources/routes_manager/1_navbar_routing/app_router.gr.dart';
 
 @RoutePage()
 class BookListPage extends StatelessWidget {
